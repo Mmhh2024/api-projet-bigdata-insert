8bc8b36f140c2e63efb63b6b11531a0cbcc077c5
hayet <hayett.makhlouf@gmail.com>
2024-05-27 11:27:03 +0200

    first commit

---
+++ src/test/java/fr/formation/api_insert_data/ApiInsertDataApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation.api_insert_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInsertDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
